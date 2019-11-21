1bedf9bc477b60ad92f3797cef68f8ac20251bbf
Dylan Hrebenach <dhrebenach@underdogmedia.com>
2019-11-21 13:40:45 -0800

    Working through initial formatting of adUnits and bidRequests. Also implemented adUnit id where like TID's have the same id

--- modules/underdogmediaAnalyticsAdapter.js
+++ modules/underdogmediaAnalyticsAdapter.js
@@ -7,7 +7,7 @@ let pbVersion = require('../package.json').version
 
 // const url = 'underdog_media_url';
 
-// Events needed
+// Prebid events needed
 const {
   EVENTS: {
     AUCTION_INIT,
@@ -19,45 +19,34 @@ const {
   }
 } = CONSTANTS;
 
+// holds auction id's for auctions currently taking place
 let currentAuctions = {}
-
-let payload = {
-  reqType: 'Prebid',
-  prebidSiteId: '',
-  prebidSiteName: '',
-  adUnits: [],
-  bidRequests: [],
-  udmInternal: {
-    prodType: 'Prebid',
-    clientVers: pbVersion
-  }
-};
 
 let underdogmediaAnalyticsAdapter = Object.assign(adapter({analyticsType: 'endpoint'}),
   {
     track({ eventType, args }) {
       try {
         if (args && args.auctionId && currentAuctions[args.auctionId] === undefined) {
-          currentAuctions[args.auctionId] = new AuctionData(this.sovrnId, args.auctionId)
+          currentAuctions[args.auctionId] = new Auction(this.prebidSiteId, this.prebidSiteName, args.auctionId)
         }
         switch (eventType) {
           case AUCTION_INIT:
-            auctionInit(args);
+            currentAuctions[args.auctionId].auctionInit(args)
             break;
           case BID_REQUESTED:
-            bidRequested(args);
+            currentAuctions[args.auctionId].bidRequested(args)
             break;
           case BID_RESPONSE:
-            bidResponse(args);
+            currentAuctions[args.auctionId].bidResponse(args)
             break;
           case BID_WON:
-            bidWon(args);
+            currentAuctions[args.auctionId].bidWon(args)
             break;
           case BID_TIMEOUT:
-            bidTimeout(args);
+            currentAuctions[args.auctionId].bidTimeout(args)
             break;
           case AUCTION_END:
-            setTimeout(function () { sendEvent(payload) }, 3100);
+            setTimeout(function () { currentAuctions[args.auctionId].send() }, 3100);
             break;
           default:
             break;
@@ -77,20 +66,89 @@ class Auction {
    */
   constructor(prebidSiteId, prebidSiteName, auctionId) {
     this.auction = {}
-    this.auction.prebidVersion = pbVersion
+    this.auction.reqType = 'Prebid'
     this.auction.prebidSiteId = prebidSiteId
     this.auction.prebidSiteName = prebidSiteName
     this.auction.auctionId = auctionId
-
     this.auction.url = utils.getTopWindowLocation().href
+    this.auction.adUnits = []
+    this.auction.bidRequests = []
+    this.auction.adUnitIdMapper = []
+    this.auction.udmInternal = {
+      prodType: 'Prebid',
+      clientVers: pbVersion
+    }
+  }
+
+  auctionInit(args) {
+
+  }
+
+  bidRequested(args) {
+    let bidReq = {
+      bidder: args.bidderCode,
+      bidType: 'Prebid',
+      bidRecvCnt: 0,
+      bidUnits: []
+    }
+    args.bids.forEach(bid => {
+      let bidUnit = {
+        adUnitId: bid.adUnitCode,
+        sizes: [{
+          won: false
+        }]
+      }
+      bidReq.bidUnits.push(bidUnit)
+      let adUnit = {}
+      let mt = ''
+      for (let key in bid.mediaTypes) {
+        mt = key
+        adUnit = {
+          id: bid.adUnitCode,
+          sizes: []
+        }
+        for (let i = 0; i < bid.mediaTypes[key].sizes.length; i++) {
+          let size = {}
+          size.w = bid.mediaTypes[key].sizes[i][0]
+          size.h = bid.mediaTypes[key].sizes[i][1]
+          let sizeStr = `${size.w}x${size.h}`
+          if (this.auction.adUnitIdMapper.includes(sizeStr)) {
+            size.id = this.auction.adUnitIdMapper.indexOf(sizeStr)
+          } else {
+            this.auction.adUnitIdMapper.push(sizeStr)
+            size.id = this.auction.adUnitIdMapper.indexOf(sizeStr)
+          }
+          size.mt = mt
+          adUnit.sizes.push(size)
+        }
+        this.auction.adUnits.push(adUnit)
+      }
+    });
+    this.auction.bidRequests.push(bidReq);
+  }
+
+  bidResponse(args) {
+
+  }
+
+  bidWon(args) {
+
+  }
+
+  bidTimeout(args) {
+
+  }
+
+  send() {
+    console.log(`this.auction: ${JSON.stringify(this.auction, null, 1)}`)
   }
 }
 
 class LogError {
   /**
    * Creates a new instance for error messages
-   * @param {object} e - error event object
-   * @param {object} data - auction specific data
+   * @param {*} e - error event object
+   * @param {*} data - auction specific data
    */
   constructor(e, data) {
     this.error = {}
@@ -98,122 +156,19 @@ class LogError {
     this.error.message = e.message
     this.error.stack = e.stack
     this.error.data = data
-    this.error.prebidVersion = $$REPO_AND_VERSION$$
-    this.error.sovrnId = sovrnId
+    this.error.prebidVersion = pbVersion
     this.error.url = utils.getTopWindowLocation().href
     this.error.userAgent = navigator.userAgent
   }
   send() {
-    console.log(`LogError error message: ${this.error}`)
+    console.log(`LogError error message: ${JSON.stringify(this.error, null, 1)}`)
   }
 }
 
-// *** EVENT HANDLERS *** //
-
-function auctionInit(args) {}
-
-let bidIdMapper = []
-
-function bidRequested(args) {
-  // Initial formatting for payload.bidRequests
-  let bidReq = {
-    bidder: args.bidderCode,
-    bidType: 'Prebid',
-    bidRecvCnt: 0,
-    bidUnits: []
-  };
-
-  // Formatting payload.adUnits
-  args.bids.forEach(bid => {
-    let bidUnit = {
-      adUnitId: bid.adUnitCode,
-      sizes: [{
-        won: false
-      }]
-    }
-    bidReq.bidUnits.push(bidUnit)
-
-    let adUnit = {}
-    let mt = ''
-
-    for (let key in bid.mediaTypes) {
-      mt = key
-      adUnit = {
-        id: bid.adUnitCode,
-        sizes: []
-      }
-      for (let i = 0; i < bid.mediaTypes[key].sizes.length; i++) {
-        let size = {}
-        size.h = bid.mediaTypes[key].sizes[i][0]
-        size.w = bid.mediaTypes[key].sizes[i][1]
-        size.mt = mt
-        adUnit.sizes.push(size)
-      }
-      payload.adUnits.push(adUnit)
-    }
-  });
-  payload.bidRequests.push(bidReq);
-}
-
-function bidResponse(args) {
-  for (let i = 0; i < payload.bidRequests.length; i++) {
-    if (args.bidderCode === payload.bidRequests[i].bidder) {
-      payload.bidRequests[i].bidRecvCnt++
-      payload.bidRequests[i].took = args.timeToRespond
-
-      for (let j = 0; j < payload.bidRequests[i].bidUnits.length; j++) {
-        if (args.adUnitCode === payload.bidRequests[i].bidUnits[j].adUnitId) {
-          payload.bidRequests[i].bidUnits[j].sizes[0].cpm = args.cpm
-          payload.bidRequests[i].bidUnits[j].sizes[0].requestId = args.requestId
-        }
-      }
-    }
-  }
-}
-
-function bidWon(args) {
-  for (let i = 0; i < payload.bidRequests.length; i++) {
-    if (args.bidderCode === payload.bidRequests[i].bidder && args.requestId === payload.bidRequests[i].bidUnits[0].sizes[0].requestId) {
-      payload.bidRequests[i].bidUnits[0].sizes[0].won = true
-      delete payload.bidRequests[i].bidUnits[0].sizes[0].requestId
-    } else {
-      delete payload.bidRequests[i].bidUnits[0].sizes[0].cpm
-      delete payload.bidRequests[i].bidUnits[0].sizes[0].requestId
-    }
-  }
-}
-
-function bidTimeout(args) {}
-
-function sendEvent(payload) {
-  try {
-    console.log(`sendEvent, payload: ${JSON.stringify(payload, null, 1)}`)
-  } catch (err) {
-    console.log(err)
-  }
-}
-
-// DEVICE TYPE
-
-// function deviceType() {
-//   if ((/ipad|android 3.0|xoom|sch-i800|playbook|tablet|kindle/i.test(navigator.userAgent.toLowerCase()))) {
-//     return 'tablet';
-//   }
-//   if ((/iphone|ipod|android|blackberry|opera|mini|windows\sce|palm|smartphone|iemobile/i.test(navigator.userAgent.toLowerCase()))) {
-//     return 'mobile';
-//   }
-//   return 'desktop';
-// }
-
-// DFP SUPPORT
-
-// let googletag = window.googletag || {};
-// googletag.cmd = googletag.cmd || [];
-// googletag.cmd.push(function() {
-//   googletag.pubads().addEventListener('slotRenderEnded', args => {
-//     console.log(Date.now() + ' GOOGLE SLOT: ' + JSON.stringify(args));
-//   });
-// });
+// utility function for debugging
+underdogmediaAnalyticsAdapter.getAuctions = () => {
+  return currentAuctions;
+};
 
 // // save the base class function
 underdogmediaAnalyticsAdapter.originEnableAnalytics = underdogmediaAnalyticsAdapter.enableAnalytics;
@@ -222,22 +177,22 @@ underdogmediaAnalyticsAdapter.originEnableAnalytics = underdogmediaAnalyticsAdap
 underdogmediaAnalyticsAdapter.enableAnalytics = function (config) {
   underdogmediaAnalyticsAdapter.initOptions = config.options;
 
+  let prebidSiteId = ''
+  let prebidSiteName = ''
+
   if (!config.options.pubId) {
     utils.logError('Publisher ID (pubId) option is not defined. Analytics won\'t work');
     return;
   }
-
-  let prebidSiteId = ''
-  let prebidSiteName = ''
-
   if (config && config.options && config.options.prebidSiteId) {
     prebidSiteId = config.options.prebidSiteId
   }
-
   if (config && config.options && config.options.prebidSiteName) {
     prebidSiteName = config.options.prebidSiteName
   }
 
+  underdogmediaAnalyticsAdapter.prebidSiteId = prebidSiteId
+  underdogmediaAnalyticsAdapter.prebidSiteName = prebidSiteName
   underdogmediaAnalyticsAdapter.originEnableAnalytics(config); // call the base class function
 }
 
